d592421ccdfd19a3b4ddf9b7e1b24a4eda918156
Clain <37398532+racoltacalin@users.noreply.github.com>
2020-11-02 11:17:14 +0200

    Add files via upload

---
+++ new_genom.py
@@ -0,0 +1,17 @@
+import flappy_bird
+import neat
+import pickle
+
+def replay_genome(config_path, genome_path="winner.pkl"):
+    # Load requried NEAT config
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    # Unpickle saved winner
+    with open(genome_path, "rb") as f:
+        genome = pickle.load(f)
+
+    # Convert loaded genome into required data structure
+    genomes = [(1, genome)]
+
+    # Call game with only the loaded genome
+    run(genomes, config)
